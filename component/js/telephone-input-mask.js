5eb68934af826e7ddffcc375d38e448bcc39e9d1
Facundo Allemand <facundo.allemand@mercadolibre.com>
2016-07-27 11:03:25 -0300

    Fix: Accept mexico cellphones.

--- component/js/telephone-input-mask.js
+++ component/js/telephone-input-mask.js
@@ -100,8 +100,16 @@ function telephoneInputMask(field, parameters) {
     // Gets the required mask based on the input value
     function getMask(data) {
         var dataCopy = data.slice();
+        var withZero = false;
+        var cellphone = false;
         if (startsWithZero(dataCopy)) {
             dataCopy.shift();
+            withZero = true;
+        }
+        var cellphoneCodeLength = startsWithCellphone(dataCopy)
+        if (cellphoneCodeLength > -1) {
+            dataCopy.splice(0,cellphoneCodeLength);
+            cellphone= true;
         }
         var codeAreaDigits = metadata.defaultAreaCodeDigits,
             i = (dataCopy.length <= metadata.maxAreaCodeDigits) ? dataCopy.length : metadata.maxAreaCodeDigits,
@@ -115,12 +123,39 @@ function telephoneInputMask(field, parameters) {
             i -= 1;
         }
         while (i >= metadata.minAreaCodeDigits && !isInAreaCodes);
-        return (startsWithZero(data)) ? metadata.masks.withZero[codeAreaDigits].split('') : metadata.masks[codeAreaDigits].split('');
+        if(withZero) {
+            if(cellphone) {
+                return metadata.masks.cellphone.withZero[codeAreaDigits].split('')
+            }
+            else {
+                return metadata.masks.withZero[codeAreaDigits].split('')
+            }
+        }
+        else {
+            if(cellphone) {
+                return metadata.masks.cellphone[codeAreaDigits].split('')
+            }
+            else {
+                return metadata.masks[codeAreaDigits].split('');
+            }
+        }
     }
 
     // Checks if telephone starts with zero
     function startsWithZero(value) {
         return (value[0] === '0');
+    }
+    
+    // Checks if telephone starts with a cellphone code
+    function startsWithCellphone(value) {
+        if(metadata.cellphoneCodes) {
+            var aux = value.slice(0, 2);
+            if(metadata.cellphoneCodes.indexOf(parseInt(aux.join(''))) > -1) {
+                return aux.length;
+            }
+        }
+        return -1;
+        
     }
 
     // Checks is char is a number
@@ -151,7 +186,16 @@ function telephoneInputMask(field, parameters) {
     // Gets maximum digits of the telephone
     function getMaxCharacters() {
         var value = stripValue(field.value);
-        return (startsWithZero(value)) ? metadata.validations.maxCharacters + 1 : metadata.validations.maxCharacters;
+        var maxCharacters = metadata.validations.maxCharacters;
+        if(startsWithZero(value)) {
+            value.shift();
+            maxCharacters += 1;
+        }
+        var cellphoneAreaCodesNumber = startsWithCellphone(value);
+        if(cellphoneAreaCodesNumber > -1) {
+            maxCharacters += cellphoneAreaCodesNumber;
+        }
+        return maxCharacters;
     }
 
     // Define all the validations
@@ -377,6 +421,7 @@ function telephoneInputMask(field, parameters) {
         var metadata = {
             'mx': {
                 codes: [33, 55, 81, 222, 223, 224, 225, 226, 227, 228, 229, 231, 232, 233, 235, 236, 237, 238, 241, 243, 244, 245, 246, 247, 248, 249, 271, 272, 273, 274, 275, 276, 278, 279, 281, 282, 283, 284, 285, 287, 288, 294, 296, 297, 311, 312, 313, 314, 315, 316, 317, 319, 321, 322, 323, 324, 325, 326, 327, 328, 329, 341, 342, 343, 344, 345, 346, 347, 348, 349, 351, 352, 353, 354, 355, 356, 357, 358, 359, 371, 372, 373, 374, 375, 376, 377, 378, 381, 382, 383, 384, 385, 386, 387, 388, 389, 391, 392, 393, 394, 395, 411, 412, 413, 414, 415, 417, 418, 419, 421, 422, 423, 424, 425, 426, 427, 428, 429, 431, 432, 433, 434, 435, 436, 437, 438, 441, 442, 443, 444, 445, 447, 448, 449, 451, 452, 453, 454, 455, 456, 457, 458, 459, 461, 462, 463, 464, 465, 466, 467, 468, 469, 471, 472, 473, 474, 475, 476, 477, 478, 481, 482, 483, 485, 486, 487, 488, 489, 492, 493, 494, 495, 496, 498, 499, 588, 591, 592, 593, 594, 595, 596, 597, 599, 612, 613, 614, 615, 616, 618, 621, 622, 623, 624, 625, 626, 627, 628, 629, 631, 632, 633, 634, 635, 636, 637, 638, 639, 641, 642, 643, 644, 645, 646, 647, 648, 649, 651, 652, 653, 656, 658, 659, 661, 662, 664, 665, 667, 668, 669, 671, 672, 673, 674, 675, 676, 677, 686, 687, 694, 695, 696, 697, 698, 711, 712, 713, 714, 715, 716, 717, 718, 719, 721, 722, 723, 724, 725, 726, 727, 728, 731, 732, 733, 734, 735, 736, 737, 738, 739, 741, 742, 743, 744, 745, 746, 747, 748, 749, 751, 753, 754, 755, 756, 757, 758, 759, 761, 762, 763, 764, 765, 766, 767, 768, 769, 771, 772, 773, 774, 775, 776, 777, 778, 779, 781, 782, 783, 784, 785, 786, 789, 791, 797, 821, 823, 824, 825, 826, 828, 829, 831, 832, 833, 834, 835, 836, 841, 842, 844, 845, 846, 861, 862, 864, 866, 867, 868, 869, 871, 872, 873, 877, 878, 891, 892, 894, 897, 899, 913, 914, 916, 917, 918, 919, 921, 922, 923, 924, 932, 933, 934, 936, 937, 938, 951, 953, 954, 958, 961, 962, 963, 964, 965, 966, 967, 968, 969, 971, 972, 981, 982, 983, 984, 985, 986, 987, 988, 991, 992, 993, 994, 995, 996, 997, 998, 999],
+                cellphoneCodes: [44, 45],
                 maxAreaCodeDigits: 3,
                 minAreaCodeDigits: 2,
                 example: '(55)4565-9876',
@@ -386,6 +431,14 @@ function telephoneInputMask(field, parameters) {
                     withZero: {
                         2: '(___)____-____',
                         3: '(____)___-____'
+                    },
+                    cellphone: {
+                        2: '(____)____-____',
+                        3: '(_____)___-____',
+                        withZero: {
+                            2: '(_____)____-____',
+                            3: '(______)___-____'
+                        }
                     }
                 },
                 defaultAreaCodeDigits: 3,
